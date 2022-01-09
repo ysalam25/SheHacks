f65dd34a3a50f4ff671ff585d978e806f80761fb
Yusra Salam <yusra.salam126@gmail.com>
2022-01-09 00:43:24 -0500

    stff

--- my-app/src/components/login/edit-profile.js
+++ my-app/src/components/login/edit-profile.js
@@ -11,24 +11,26 @@ export class EditProfile extends React.Component {
                 <div className="edit-content">
                     <div className= "edit-form">
                         <div className="editform-group">
+                        <label>student ID:</label>
                         <input type="text" name="studentID" placeholder="eg. 123456"></input>
                         </div>
                         <div className="editform-group">
+                        <label>email:</label>
                         <input type="email" name="email" placeholder="eg. janedoe@gmail.com"></input>
                         </div>
                         <div className="editform-group">
+                        <label>university:</label>
                         <input type="text" name="university" placeholder="eg. Western University"></input>
                         </div>
                         <div className="editform-group">
-                            show sisters from
-                        <input type="text" name="university" placeholder="eg. Western University"></input>
+                            <label>show sisters from:</label>
+                            <input type="radio" value="my university" name="uni" /> My University
+                            <input type="radio" value="specific universities" name="specific" /> Specific University
+                            <input type="radio" value="anywhere" name="anywhere" /> Anywhere
                         </div>
                     </div>   
                 </div>
-                <div className="home-footer">
-                    <button type="button" className="profilebtn">Profile Settings</button>
-                    <button type="button" className="editbtn">Edit Tags</button>
-                    <button type="button" className="connectbtn">Connect to Sisters</button>
+                <div className="edit-footer">
                 </div>
             
         </div>
